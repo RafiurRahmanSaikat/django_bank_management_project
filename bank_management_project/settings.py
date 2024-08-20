69409e83e5e8953c1559f5df584c9f9f79c89661
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2024-08-20 16:58:42 +0600

    Cross Origin  Added

--- bank_management_project/settings.py
+++ bank_management_project/settings.py
@@ -20,7 +20,10 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://bankmanagement-ynnk.onrender.com/",
+    "https://*.127.0.0.1",
+]
 
 # Application definition
 
